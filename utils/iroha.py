8b6e9c3fb1d28a1d9b157cdd22aba540e6a476f7
David <mitrailer@gmail.com>
2019-09-24 13:10:10 -0500

    global functions for nodes. Node can set details to other nodes if the have the permission to do so

--- utils/iroha.py
+++ utils/iroha.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from iroha import IrohaCrypto
+from iroha import IrohaCrypto, Iroha, IrohaGrpc
 import binascii
 import sys
 if sys.version_info[0] < 3:
@@ -36,3 +36,32 @@ def send_transaction_and_print_status(transaction, network):
     network.send_tx(transaction)
     for status in network.tx_status_stream(transaction):
         print(status)
+
+
+# functions available to all users
+def set_detail_to_node(sender, receiver, private_key, detail_key, detail_value, domain, ip):
+    """
+    Set the details of a node. In federated learning the details are in JSON format and
+    contains the address (location) where the weight is stored (if the weight is small enough it can be
+    embedded to the block if needed)
+    :param sender: (str) name of the node sending the information
+    :param receiver: (str) name of the node receiving the information
+    :param private_key: (str) Private key of the user
+    :param detail_key: (str) Name of the detail we want to set
+    :param detail_value: (str) Value of the detail
+    :param domain: (str) Name of the domain
+    :param ip: (ip) address for connecting to the BSMD
+    """
+    account = sender + '@' + domain
+    iroha = Iroha(account)
+    account_id = receiver + '@' + domain
+    ip_address = ip + ':50051'
+    network = IrohaGrpc(ip_address)
+    tx = iroha.transaction([
+        iroha.command('SetAccountDetail',
+                      account_id=account_id,
+                      key=detail_key,
+                      value=detail_value)
+    ])
+    IrohaCrypto.sign_transaction(tx, private_key)
+    send_transaction_and_print_status(tx, network)
