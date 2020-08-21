9b24f62e4ecb769bc04e50eaf01ea34a8bc49fb0
David Lopez <dmitrailer@gmail.com>
2020-08-21 13:31:07 -0500

    add documentation

--- security/risk_assessment_framework/functions.py
+++ security/risk_assessment_framework/functions.py
@@ -4,13 +4,29 @@ import csv
 
 
 def get_threats_occurrence_probability(threats):
+    """
+    Compute the probability of occurrence of identified threats for each attack goal and scenario. See Tables 7-11 in
+    the paper
+    :param threats: data frame containing the threats for each attack goal and scenario.
+    :return: dataframe containing the probability of occurrence of identified threats for each attack goal and scenario
+    """
     threats['un-normalized_probability'] = threats.apply(lambda row: row.capacity + row.opportunity + row.motivation,
                                                          axis=1)
     return threats
 
 
 def get_combined_risk_assessment(threats, impact_by_attack, attack_goal, scenario, impact):
-
+    """
+    Compute the the combined risk assessment of the attack goal at the scenario with the selected impact. See table 12
+    in the paper
+    :param threats: dataframe containing the probability of occurrence of identified threats for each attack goal
+                    and scenario
+    :param impact_by_attack: dataframe containing the impacts for each attack goal
+    :param attack_goal: attack goal
+    :param scenario: scenario
+    :param impact: impact on the attack goal
+    :return: the combined risk assessment of the attack goal at the scenario with the selected impact
+    """
     p_r_max = threats.query('attack_goal==@attack_goal and scenario==@scenario')['un-normalized_probability'].max()
     i_m = impact_by_attack.query('attack_goals==@attack_goal')[impact]
     combined_risk_assessment = i_m * p_r_max
@@ -22,6 +38,11 @@ def get_combined_risk_assessment(threats, impact_by_attack, attack_goal, scenari
 
 
 def get_risk_characterization(combined_risk_assessment):
+    """
+    Compute the risk characterization for the combined risk assessment. See table 12
+    :param combined_risk_assessment: combined risk assessment of the attack goal at the scenario with the selected impact
+    :return: risk characterization for the combined risk assessment
+    """
     if combined_risk_assessment >= 36:
         risk_treatment = 'refuse'
     elif 24 <= combined_risk_assessment < 36:
@@ -34,6 +55,14 @@ def get_risk_characterization(combined_risk_assessment):
 
 
 def get_risk_characterization_table(threats, impact_by_attack):
+    """
+    Compute the risk characterization table for all attack goals at all scenarios and impacts. See table 13
+    in the paper
+    :param threats: dataframe containing the probability of occurrence of identified threats for each attack goal
+                    and scenario
+    :param impact_by_attack: dataframe containing the impacts for each attack goal
+    :return: Risk characterization table
+    """
     attack_goals = ['G1', 'G2', 'G3', 'G4', 'G5']
     scenarios = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'S14', 'S15', 'S16',
                  'S17', 'S18', 'S19', 'S20', 'S21', 'S22']
